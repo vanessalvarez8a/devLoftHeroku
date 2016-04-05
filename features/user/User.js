87436fabef0dc74e70d5b49c2859d91cd285da2f
vanessalvarez8a <vanessalvarez8a@gmail.com>
2016-04-05 17:52:20 -0500

    finone

--- features/user/User.js
+++ features/user/User.js
@@ -10,7 +10,7 @@ var User = new mongoose.Schema({
   name: {type: String, set: capitalize, required: true},
   email: {type: String},
   githubId: {type: String},
-  projects: [{type: mongoose.Schema.Types.ObjectId, ref: 'Project'}]
+  projects: [{type: Mongoose.Schema.ObjectId, ref: 'Project'}]
 
 }, {collection: 'user'}) //collection is the projects
 
