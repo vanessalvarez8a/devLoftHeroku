400faad61e92bfa884ffeab933cf397ed697c065
vanessalvarez8a <vanessalvarez8a@gmail.com>
2016-04-05 00:10:17 -0500

    changes

--- public/js/controllers/showHeaderCtrl.js
+++ public/js/controllers/showHeaderCtrl.js
@@ -1,12 +1,12 @@
 angular.module('devLoftApp').controller('showHeaderCtrl', function($scope, $rootScope) {
 
-  $scope.$on.showHeader = true;
+  $scope.showHeader = true;
 
   // $scope.$on('$stateChangeStart', function(event, toState) {
 
-  $scope.$emit('$stateChangeStart', function(event, toState) {
+  $scope.$on('$stateChangeStart', function(event, toState) {
     if (toState.name === 'home') {
-      $scope.showHeader = true;
+      return $scope.showHeader = true;
     } else {
       $scope.showHeader = false;
     }
