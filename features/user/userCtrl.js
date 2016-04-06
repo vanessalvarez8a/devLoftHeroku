86061ecca284ab95ff3e2660856a32d8132ff6ff
vanessalvarez8a <vanessalvarez8a@gmail.com>
2016-04-06 13:39:57 -0500

    getOneProject changes

--- features/user/userCtrl.js
+++ features/user/userCtrl.js
@@ -11,7 +11,7 @@ module.exports = {
     })
   },
   getOneUser: function(req, res) {
-    User.findOne({_id:req.params.id}).populate('projects').exec().then(function( user ) {
+    User.findOne({"_id":req.params.id}).populate('projects').exec().then(function( user ) {
       res.send(user);
     })
   },
