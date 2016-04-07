6f3b648e507a2ef4e6aee18cb976b8742e5cb7b3
vanessalvarez8a <vanessalvarez8a@gmail.com>
2016-04-06 23:03:42 -0500

    added controller to the panels

--- public/js/controllers/panelCtrl.js
+++ public/js/controllers/panelCtrl.js
@@ -3,7 +3,7 @@ angular.module('devLoftApp')
 
   $scope.this.tab = 1;
 
-  $scope.this.selectTab = function(setTab) {
+  $scope.this.selectTab = function(tab, setTab) {
     this.tab = setTab;
   };
 
